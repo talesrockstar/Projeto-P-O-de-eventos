cddfc38b43e2813a5d7c76cbdd13754de1c7ab31
Heitor-Braz <heitorbrazcm@gmail.com>
2025-05-28 12:14:36 -0300

    Código 100% completo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -22,5 +22,40 @@ describe('teste página de login', () => {
 describe('teste página de cadastro', () => {
   it('cadastro', () => {
     cy.visit('cadastro.html')
+
+    cy.get('.endereco').type('Rua Exemplo, 123')
+
+    cy.get('.telefone').type('71999999999')
+
+    cy.get('.nome').type('João da Silva')
+
+    cy.get('.email').type('wdwag@example.com')
+
+    cy.get('.senha').type('12345678')
+
+    cy.get('.change-photo').click()
+
+    cy.get('.btn').click()
+})
+it('não preenchido', () => {
+  cy.visit('cadastro.html')
+  cy.get('#endereco').should('have.value', '')
+  cy.get('#telefone').should('have.value', '')
+  cy.get('#nome').should('have.value', '')
+  cy.get('#exampleInputEmail1').should('have.value', '')
+  cy.get('#exampleInputPassword1').should('have.value', '')
+  cy.get('.btn').click() 
+})
+})
+describe('teste página de redefinição de senha', () => {
+  it('redefinir senha', () => {
+    cy.visit('redefinirsenha.html')
+    cy.get('.email').type('wdwag@example.com')
+    cy.get('.btn').click()
+})
+it('não preenchido', () => {
+  cy.visit('redefinirsenha.html')
+  cy.get('#exampleInputEmail1').should('have.value', '')
+  cy.get('.btn').click()
 })
 })
